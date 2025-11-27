acdf8941382061fe38bc620a36bdfcba6ba037a4
irjakniibos <irza.yaumil.ft23@mail.umy.ac.id>
2025-11-27 12:59:57 +0700

    Menambahkan interface SiswaDao dengan metode untuk mengambil semua data siswa dan menyisipkan data siswa baru.

---
+++ app/src/main/java/com/example/a20230140094_roomdatabase/room/SiswaDao.kt
@@ -0,0 +1,17 @@
+package com.example.a20230140094_roomdatabase.room
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface SiswaDao {
+    @Query("SELECT * from tbl_siswa ORDER BY nama ASC")
+    fun getAllSiswa(): Flow<List<Siswa>>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(siswa: Siswa)
+
+}
