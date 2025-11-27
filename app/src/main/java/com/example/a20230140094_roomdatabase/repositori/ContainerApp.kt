970f3b43994cfb2f9ac7b852de1463bd9162a97a
irjakniibos <irza.yaumil.ft23@mail.umy.ac.id>
2025-11-27 13:18:27 +0700

    Menambahkan implementasi ContainerDataApp untuk menginisialisasi OfflineRepositoriSiswa.

--- app/src/main/java/com/example/a20230140094_roomdatabase/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/a20230140094_roomdatabase/repositori/ContainerApp.kt
@@ -1,6 +1,14 @@
 package com.example.a20230140094_roomdatabase.repositori
 
+import android.content.Context
+import com.example.a20230140094_roomdatabase.room.DatabaseSiswa
+
 interface containerApp{
     val repositoriSiswa : RepositoriSiswa
 }
 
+class ContainerDataApp(private val context: Context): containerApp{
+    override val repositoriSiswa: RepositoriSiswa by lazy {
+        OfflineRepositoriSiswa(DatabaseSiswa.getDatabase(context).siswaDao())
+    }
+}
