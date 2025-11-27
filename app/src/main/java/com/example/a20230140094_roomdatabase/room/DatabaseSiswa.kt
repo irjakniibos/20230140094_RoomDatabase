80c70f892222593c87c606f76043b624b8401d79
irjakniibos <irza.yaumil.ft23@mail.umy.ac.id>
2025-11-27 13:03:57 +0700

    Menambahkan DatabaseSiswa yang mewarisi RoomDatabase.

---
+++ app/src/main/java/com/example/a20230140094_roomdatabase/room/DatabaseSiswa.kt
@@ -0,0 +1,9 @@
+package com.example.a20230140094_roomdatabase.room
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(entities = [Siswa::class], version = 1, exportSchema = false)
+abstract class DatabaseSiswa : RoomDatabase() {
+
+}
